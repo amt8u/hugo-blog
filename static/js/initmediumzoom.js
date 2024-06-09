27bef10a39f49d1f2e5d01a0592e785e5187b5b5
amt8u <amt8u@amt8u.com>
2024-06-10 00:13:42 +0530

    Script not getting included in the pages when directly using in baseof.html. Works on local server, but not on production deployement

--- static/js/initmediumzoom.js
+++ static/js/initmediumzoom.js
@@ -1,5 +1,7 @@
-let darkMode = false;
-if (!document.getElementById("darkModeStyle")?.disabled) {
-    darkMode = true;
+window.onload = () => {
+    let darkMode = false;
+    if (!document.getElementById("darkModeStyle")?.disabled) {
+        darkMode = true;
+    }
+    window.mediumZoomInstance = mediumZoom('img', {background: darkMode ? "#000" : "#f7f7f1"});
 }
-window.mediumZoomInstance = mediumZoom('img', {background: darkMode ? "#000" : "#f7f7f1"});
